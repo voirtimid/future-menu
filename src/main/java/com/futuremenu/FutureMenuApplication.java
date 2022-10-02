17e4793fd6ff83e97d930b8dbfb89686fad51ed8
Oliver Dimitriov <oliver.dimitriov@gmail.com>
2022-10-02 12:17:06 +0200

    Initial commit

---
+++ src/main/java/com/futuremenu/FutureMenuApplication.java
@@ -0,0 +1,13 @@
+package com.futuremenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futuremenu/FutureMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuremenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
